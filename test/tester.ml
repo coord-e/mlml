8a5e9ead03ea75e56f4d33826b21ca836692ec02
coord.e <me@coord-e.com>
2019-04-05 14:57:34 +0000

    Add: Add a test case which compares output of mlml and ocaml

---
+++ test/tester.ml
@@ -0,0 +1,26 @@
+open Mlml
+
+let exec_with_mlml source =
+  let as_file = Filename.temp_file "." ".s" in
+  let oc = open_out as_file in
+  Printf.fprintf oc "%s\n" @@ Compile.compile source;
+  close_out oc;
+  let exec_file = Filename.temp_file "." "" in
+  let ret_code = Sys.command @@ Printf.sprintf "gcc %s -o %s" as_file exec_file in
+  if ret_code != 0 then failwith "Failed to compile resulting assembly";
+  Sys.command @@ exec_file
+;;
+
+let exec_with_ocaml source =
+  let ml_file = Filename.temp_file "." ".ml" in
+  let oc = open_out ml_file in
+  Printf.fprintf oc "exit (%s)\n" source;
+  close_out oc;
+  Sys.command @@ "ocaml " ^ ml_file
+;;
+
+let f source =
+  let mlml_result = exec_with_mlml source in
+  let ocaml_result = exec_with_ocaml source in
+  assert (mlml_result = ocaml_result)
+;;
