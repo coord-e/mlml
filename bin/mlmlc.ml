93c3eb56f54ebdd8a6b370c2ac664630201279bc
coord.e <me@coord-e.com>
2019-04-05 14:56:20 +0000

    Change: Return asm string from Compile.compile

--- bin/mlmlc.ml
+++ bin/mlmlc.ml
@@ -2,6 +2,6 @@ open Mlml
 
 let () =
   match Sys.argv with
-  | [|_; source|] -> Compile.compile source
+  | [|_; source|] -> Compile.compile source |> print_endline
   | _ -> failwith "Invalid number of arguments"
 ;;
--- lib/compile.ml
+++ lib/compile.ml
@@ -1,5 +1,1 @@
-let compile source =
-  (* Lexer.tokenize source |> Lexer.tokens_to_string |> print_endline *)
-  (* Lexer.tokenize source |> Parser.parse |> Parser.string_of_ast |> print_endline *)
-  Lexer.tokenize source |> Parser.parse |> Codegen.codegen |> print_endline
-;;
+let compile source = Lexer.tokenize source |> Parser.parse |> Codegen.codegen
