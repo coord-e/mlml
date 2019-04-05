eb52fc34aebc3ca90b1c11befdadb47e223ecc7d
coord.e <me@coord-e.com>
2019-04-05 07:14:27 +0000

    Add: Parse let expression

--- parser.ml
+++ parser.ml
@@ -4,6 +4,7 @@ type ast =
   | Int of int
   | Add of ast * ast
   | Mul of ast * ast
+  | LetVar of string * ast * ast
 
 let parse_literal tokens =
   match tokens with
@@ -31,11 +32,26 @@ let parse_add tokens =
     | _ -> (tokens, lhs)
   in aux lhs tokens
 
+let rec parse_let = function
+  | L.Let :: L.LowerIdent ident :: L.Equal :: rest -> (
+    let rest, lhs = parse_expression rest in
+    match rest with
+    | L.In :: rest -> (
+      let rest, rhs = parse_expression rest in
+      (rest, LetVar (ident, lhs, rhs))
+    )
+    | _ -> failwith "could not find 'in'"
+  )
+  | tokens -> parse_add tokens
+
+and parse_expression tokens = parse_let tokens
+
 let parse tokens =
-  let _rest, ast = parse_add tokens in
+  let _rest, ast = parse_expression tokens in
   ast
 
 let rec ast_to_string = function
   | Int num -> Printf.sprintf "Int %d" num
   | Add (lhs, rhs) -> Printf.sprintf "Add (%s) (%s)" (ast_to_string lhs) (ast_to_string rhs)
   | Mul (lhs, rhs) -> Printf.sprintf "Mul (%s) (%s)" (ast_to_string lhs) (ast_to_string rhs)
+  | LetVar (ident, lhs, rhs) -> Printf.sprintf "Let (%s) (%s) (%s)" ident (ast_to_string lhs) (ast_to_string rhs)
