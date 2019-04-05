5e57e9f404c8f8eee30aad444f449c22b059fcd9
coord.e <me@coord-e.com>
2019-04-05 15:08:01 +0000

    Change: Write long test source using quoted string literal

--- test/simple_func.ml
+++ test/simple_func.ml
@@ -1,4 +1,8 @@
 let () = (
   Tester.f "let f x = x + 3 in f 40";
-  Tester.f "let f x = x + 3 in let g x = 10 * x in f (g 4)"
+  Tester.f {|
+    let f x = x + 3 in
+    let g x = 10 * x in
+    f (g 4)
+  |}
 )
