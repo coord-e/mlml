be25314e5594befef26102e5a5a973b266d9ff55
coord.e <me@coord-e.com>
2019-04-05 06:44:30 +0000

    Add: Tokenize '='

--- lexer.ml
+++ lexer.ml
@@ -50,6 +50,7 @@ let rec tokenize_aux acc rest =
     )
     | '+' -> tokenize_aux (Plus :: acc) t
     | '*' -> tokenize_aux (Star :: acc) t
+    | '=' -> tokenize_aux (Equal :: acc) t
     | _ -> failwith @@ Printf.sprintf "unexpected character: '%c'" h
 
 let explode s =
