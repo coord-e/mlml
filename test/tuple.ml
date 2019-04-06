e63eff39384996bdbf8405342ad47780dbcd2ba8
coord.e <me@coord-e.com>
2019-04-06 11:53:32 +0000

    Add: Test: Add test cases

---
+++ test/tuple.ml
@@ -0,0 +1,18 @@
+let () =
+  Tester.f "let a, b = 1, 2 in a + b";
+  Tester.f {|
+    let a = 1, 2, 3 in
+    let x, y, z = a in
+    x * y + z
+  |};
+  Tester.f
+    {|
+    let f a = a, a+1, a+2, a+3 in
+    let a, b, c, d = f 10 in
+    a + b + c + d
+  |};
+  Tester.f {|
+    let add (a, b) = a + b in
+    add (1, 2)
+  |}
+;;
