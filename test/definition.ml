554dbded17206da249e61008b058e88f52f69513
coord.e <me@coord-e.com>
2019-04-07 06:06:39 +0000

    Add: Test: Add one more test case

--- test/definition.ml
+++ test/definition.ml
@@ -9,6 +9,12 @@ let var = 1 ;;
 print_int var
   |};
   Tester.f {|
+let var = 1
+let f a = a * 2
+let d = f var ;;
+print_int var
+  |};
+  Tester.f {|
 let f a = a + 1 ;;
 let v = f 2 ;;
 let g x = print_int x ;;
