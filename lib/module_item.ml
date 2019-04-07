5d9cf760adf4c2e86f0687dc3bdfa2f8e3909a59
coord.e <me@coord-e.com>
2019-04-07 03:31:27 +0000

    Add: Parser: Add a parser of module items

---
+++ lib/module_item.ml
@@ -0,0 +1,38 @@
+(* Parse the module items.                                               *)
+(* https://caml.inria.fr/pub/docs/manual-ocaml/modules.html#module-items *)
+
+module Def = Definition
+module Expr = Expression
+
+type module_item =
+  | Definition of Def.t
+  | Expression of Expr.t
+
+type t = module_item list
+
+let rec parse_module_items = function
+  | L.DoubleSemicolon :: rest -> parse_module_items rest
+  | tokens ->
+    let rest, def_opt = Def.try_parse_definition tokens in
+    (match def_opt with
+    | Some def ->
+      let rest, items = parse_module_items rest in
+      rest, Definition def :: items
+    | None ->
+      (* may fail in parse_expression (OK because there's no other candidate) *)
+      let rest, expr = Expr.parse_expression rest in
+      let rest, items = parse_module_items rest in
+      rest, Expression expr :: items)
+;;
+
+let string_of_module_item = function
+  | Definition def -> Def.string_of_definition def
+  | Expression expr -> Expr.string_of_expression expr
+;;
+
+(* TODO: function composition can make this clearer *)
+let string_of_module_items items =
+  List.map string_of_module_item items |> List.concat ";; "
+;;
+
+let f = parse_module_items
--- lib/parser.ml
+++ lib/parser.ml
@@ -1,11 +1,14 @@
-module Expr = Expression
+(* Parse the compilation unit.                                                   *)
+(* https://caml.inria.fr/pub/docs/manual-ocaml/compunit.html#unit-implementation *)
 
-type t = Expr.t
-type ast = Expr.t
+module Item = Module_item
+
+type ast = Item.t
+type t = ast
 
 let f tokens =
-  let _rest, ast = Expr.f tokens in
+  let _rest, ast = Item.f tokens in
   ast
 ;;
 
-let string_of_ast = Expr.string_of_expression
+let string_of_ast = Item.string_of_module_items
