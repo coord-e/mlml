9b1907541328ae05e9d3576381f02bde67f73f0e
coord.e <me@coord-e.com>
2019-04-08 16:50:28 +0000

    Add: Analysis: Add closure conversion of definition

--- lib/analysis/analysis.ml
+++ lib/analysis/analysis.ml
@@ -3,7 +3,7 @@ module Item = P.Module_item
 
 let conv_module_item = function
   | Item.Expression expr -> Item.Expression (Closure.closure_conversion expr)
-  | i -> i
+  | Item.Definition defn -> Item.Definition (Closure.closure_conversion_defn defn)
 ;;
 
 let f = List.map conv_module_item
