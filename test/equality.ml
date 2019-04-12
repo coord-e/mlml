e5c4bf70bf9e5c37f0d6e1c32ace37e58288b153
coord.e <me@coord-e.com>
2019-04-12 11:43:50 +0000

    Add: Test: Add more test cases

--- test/equality.ml
+++ test/equality.ml
@@ -15,6 +15,14 @@ let () =
   Tester.bool_expr "((2, 3), 5, (2, 6)) <> ((2, 3), 5, (2, 6))";
   Tester.bool_expr "(1, 5, (2, 6)) <> (1, 5, (2, 3))";
   Tester.bool_expr "(4, 3) <> (2, 3)";
+  Tester.bool_expr
+    {|
+let a = (4, (3, (3, 2), 3)) in
+let b = (3, 2) in
+let f v = (4, (v, b, v)) in
+a = f 3
+|};
+  (* variant *)
   Tester.f
     {|
 type variant =
@@ -42,5 +50,24 @@ print_bool (A (5, 2) <> C 1) ;;
 print_bool (A (0, 0) = D) ;;
 print_bool (D = D) ;;
 print_bool (D <> C 0) ;;
+
+(* with tuple *)
+print_bool ((1, A (1, 2)) = (1, A (1, 2))) ;;
+print_bool ((A (1, 2), 4) <> (B (1, 2), 4)) ;;
+print_bool ((1, D) = (1, C 0)) ;;
+
+(* complex *)
+let f = function
+  | 1 -> A (0, 0)
+  | 2 -> B (0, 0)
+  | x -> C x
+in let g = function
+  | x, y when x = (1, 2) -> y + 1
+  | x, y when x = (3, 5) -> y + 3
+  | _, y -> y
+in
+print_bool (f (g ((3, 5), 0)) = A (0, 0));
+print_bool (f (g ((1, 5), 5)) <> C 5);
+print_bool (f (g ((1, 2), 1)) = B (0, 0))
   |}
 ;;
