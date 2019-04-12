2a87b14cfd7430ddeb60e9adae51ee4669c03899
coord.e <me@coord-e.com>
2019-04-12 11:30:34 +0000

    Add: Test: Add a test case to compare variants

--- test/equality.ml
+++ test/equality.ml
@@ -5,7 +5,6 @@ let () =
   Tester.bool_expr "0 == 1";
   Tester.bool_expr "0 != 1";
   Tester.bool_expr "(1, 2, 3) == (1, 2, 3)";
-
   (* Structural equality *)
   Tester.bool_expr "2 = 2";
   Tester.bool_expr "0 <> 0";
@@ -16,3 +15,32 @@ let () =
   Tester.bool_expr "((2, 3), 5, (2, 6)) <> ((2, 3), 5, (2, 6))";
   Tester.bool_expr "(1, 5, (2, 6)) <> (1, 5, (2, 3))";
   Tester.bool_expr "(4, 3) <> (2, 3)";
+  Tester.f
+    {|
+type variant =
+  | A of int * int
+  | B of int * int
+  | C of int
+  | D
+
+let print_bool = function
+  | true -> print_int 1
+  | false -> print_int 0
+;;
+
+(* Physical equality *)
+print_bool (A (1, 2) == A (1, 2)) ;;
+print_bool (A (6, 2) != B (6, 2)) ;;
+
+(* Structural equality *)
+print_bool (A (1, 2) = A (1, 2)) ;;
+print_bool (A (3, 2) <> A (1, 4)) ;;
+print_bool (A (3, 2) = A (1, 4)) ;;
+print_bool (A (6, 2) = B (6, 2)) ;;
+print_bool (C 1 = C 1) ;;
+print_bool (A (5, 2) <> C 1) ;;
+print_bool (A (0, 0) = D) ;;
+print_bool (D = D) ;;
+print_bool (D <> C 0) ;;
+  |}
+;;
