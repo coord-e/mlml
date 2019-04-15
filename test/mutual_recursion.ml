47cfbf64f6012a3e6492b318b244cace0a48f703
coord.e <me@coord-e.com>
2019-04-15 14:59:25 +0000

    Add: Test: Add test cases for definitions

--- test/mutual_recursion.ml
+++ test/mutual_recursion.ml
@@ -29,5 +29,28 @@ let f x = x + 1
 and a, b = 1, 2
 and g x = x * 10
 in f a + g b
+  |};
+  (* definiton version *)
+  Tester.f
+    {|
+let rec is_even = function
+  | 0 -> true
+  | x -> is_odd (x-1)
+and is_odd = function
+  | 0 -> false
+  | x -> is_even (x-1)
+and print_bool = function
+  | true -> print_int 1
+  | false -> print_int 0
+;;
+print_bool (is_even 13)
+  |};
+  Tester.f
+    {|
+let f x = x + 1
+and a, b = 1, 2
+and g x = x * 10
+;;
+print_int (f a + g b)
   |}
 ;;
