2181407019dc78ee88ef1085a3b839b980682a1d
coord.e <me@coord-e.com>
2019-04-17 14:29:42 +0000

    Change: Codegen: Implement output buffer using list

--- lib/codegen/output_buffer.ml
+++ lib/codegen/output_buffer.ml
@@ -1,20 +1,12 @@
 type t =
-  { main : Buffer.t
-  ; sub : Buffer.t }
+  { mutable main : string list
+  ; mutable sub : string list }
 
-let create size = {main = Buffer.create size; sub = Buffer.create size}
+let create () = {main = []; sub = []}
 
 let emit_instruction buf inst =
-  Buffer.add_char buf.main '\t';
-  Buffer.add_string buf.main inst;
-  Buffer.add_char buf.main '\n'
+  let s = Printf.sprintf "\t%s" inst in
+  buf.main <- s :: buf.main
 ;;
 
-let length buf = Buffer.length buf.main + Buffer.length buf.sub
-
-let contents buf =
-  let b = Buffer.create (length buf) in
-  Buffer.add_buffer b buf.sub;
-  Buffer.add_buffer b buf.main;
-  Buffer.contents b
-;;
+let contents buf = List.append buf.sub buf.main |> String.concat "\n"
