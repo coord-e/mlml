4e2ba1b9ee1637284e0e873368c451bf6af50e49
coord.e <me@coord-e.com>
2019-04-20 09:01:31 +0000

    Add: Test: Add test cases

---
+++ test/string.ml
@@ -0,0 +1,82 @@
+let () =
+  Tester.f {|print_string "hello, world!"|};
+  Tester.f {|
+let s1 = "hello" in
+let s2 = "world" in
+print_string (s1 ^ ", " ^ s2)
+|};
+  Tester.bool_expr
+    {|
+  let s1 = "hel" in
+  let is_hello = function "hello" -> true | _ -> false in
+  is_hello (s1 ^ "lo")
+|};
+  Tester.bool_expr {|
+  "hello" = "hello"
+  |};
+  Tester.bool_expr {|
+  "hell" <> "hello"
+  |};
+  Tester.bool_expr {|
+  "hello" != "hello"
+  |};
+  Tester.f
+    {|
+let f = function 'a' -> "AHHHH" | 'b' -> "BEEEE" | _ -> "huh, idk" in
+print_string (f "aieee".[0])
+|};
+  Tester.f
+    {|
+(* TODO: Use String.length *)
+(* TODO: Use > comparison *)
+let explode l s =
+  let rec exp i l = if (i + 1) = 0 then l else exp (i - 1) (s.[i] :: l) in
+  exp (l - 1) []
+;;
+
+let rec print = function
+  | [] -> print_string "end"
+  | h :: t ->
+      print_char h;
+      print_string "->";
+      print t
+;;
+
+print (explode 12 "hello, world")
+  |};
+  Tester.f
+    {|
+type op =
+  | Just of string
+  | Concat of op * op
+;;
+
+let replace = function
+  | "like" -> "love"
+  | "bad" -> "cool"
+  | s -> s
+;;
+
+let rec f = function
+  | Just s -> replace s
+  | Concat (a, b) -> f a ^ " " ^ f b
+;;
+
+let s = Concat (
+  Concat (
+    Just "I",
+    Just "like"
+  ),
+  Concat (
+    Concat (
+      Just "bad",
+      Concat (
+        Just "and",
+        Just "cute"
+      )
+    ),
+    Just "dogs"
+  )
+) in print_string (f s)
+  |}
+;;
