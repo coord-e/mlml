16dd615538fb813941dd7d441a2c54921a33c2af
coord.e <me@coord-e.com>
2019-04-20 13:20:00 +0000

    Add: Parser: Parse record definition

--- lib/parser/definition.ml
+++ lib/parser/definition.ml
@@ -6,30 +6,56 @@ module Pat = Pattern
 module TyExpr = Type_expression
 module L = Lexer
 
-type type_def = Variant of (string * TyExpr.t option) list
+type type_def =
+  | Variant of (string * TyExpr.t option) list
+  | Record of (string * TyExpr.t) list
 
 type t =
   | LetAnd of bool * Expr.let_binding list
   | TypeDef of string * type_def
 
+let parse_variant tokens =
+  let rec aux = function
+    | L.CapitalIdent name :: L.Of :: rest ->
+      let rest, ty_expr = TyExpr.parse_type_expression rest in
+      (match rest with
+      | L.Vertical :: rest ->
+        let rest, acc = aux rest in
+        rest, (name, Some ty_expr) :: acc
+      | _ -> rest, [name, Some ty_expr])
+    | L.CapitalIdent name :: L.Vertical :: rest ->
+      let rest, acc = aux rest in
+      rest, (name, None) :: acc
+    | L.CapitalIdent name :: rest -> rest, [name, None]
+    | rest -> rest, []
+  in
+  let rest, ctors = match tokens with L.Vertical :: rest | rest -> aux rest in
+  rest, Variant ctors
+;;
+
+let parse_record tokens =
+  let rec aux = function
+    | L.LowerIdent name :: L.Colon :: rest ->
+      let rest, ty_expr = TyExpr.parse_type_expression rest in
+      (match rest with
+      | L.Semicolon :: rest ->
+        let rest, acc = aux rest in
+        rest, (name, ty_expr) :: acc
+      | _ -> rest, [name, ty_expr])
+    | rest -> rest, []
+  in
+  let rest, fields = match tokens with L.LBrace :: rest | rest -> aux rest in
+  match rest with
+  | L.RBrace :: rest -> rest, Record fields
+  | _ -> failwith "record definition is not terminated"
+;;
+
 let try_parse_type = function
   | L.LowerIdent ident :: L.Equal :: rest ->
-    let rec aux = function
-      | L.CapitalIdent name :: L.Of :: rest ->
-        let rest, ty_expr = TyExpr.parse_type_expression rest in
-        (match rest with
-        | L.Vertical :: rest ->
-          let rest, acc = aux rest in
-          rest, (name, Some ty_expr) :: acc
-        | _ -> rest, [name, Some ty_expr])
-      | L.CapitalIdent name :: L.Vertical :: rest ->
-        let rest, acc = aux rest in
-        rest, (name, None) :: acc
-      | L.CapitalIdent name :: rest -> rest, [name, None]
-      | _ -> rest, []
+    let rest, def =
+      match rest with L.LBrace :: _ -> parse_record rest | _ -> parse_variant rest
     in
-    let rest, ctors = match rest with L.Vertical :: rest | rest -> aux rest in
-    rest, Some (TypeDef (ident, Variant ctors))
+    rest, Some (TypeDef (ident, def))
   | tokens -> tokens, None
 ;;
 
@@ -63,6 +89,11 @@ let string_of_type_def = function
       | None -> ctor
     in
     List.map aux variants |> String.concat " | "
+  | Record fields ->
+    let aux (name, ty) =
+      Printf.sprintf "%s: %s" name (TyExpr.string_of_type_expression ty)
+    in
+    List.map aux fields |> String.concat "; " |> Printf.sprintf "{%s}"
 ;;
 
 let string_of_definition = function
