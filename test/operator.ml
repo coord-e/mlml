b5fccdc0f9917b202e980da759d7a3f6507528fd
coord.e <me@coord-e.com>
2019-04-29 15:40:21 +0000

    Add: Test: Add a test case of user-defined operators

--- test/operator.ml
+++ test/operator.ml
@@ -22,5 +22,16 @@ print_list (l1 @ l2);
 print_list (l1 @ [1; 3]);
 print_list (l2 @ [10]);
 print_list ([4; 5] @ [3; 4; 10])
+  |};
+  (* user-defined *)
+  Tester.f
+    {|
+let (<=>) a b = a - b
+let (-->) a b = a * b
+let (@~@) a b = a + b
+;;
+print_int (50 <=> 8);
+print_int (7 --> 6);
+print_int (13 @~@ 29)
   |}
 ;;
