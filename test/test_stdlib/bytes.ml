f87e35d2fed35fc762b4572a87200bc808358c24
coord.e <me@coord-e.com>
2019-04-30 09:20:35 +0000

    Add: Stdlib: Implement Bytes.of_string, to_string, get, set

---
+++ test/test_stdlib/bytes.ml
@@ -0,0 +1,7 @@
+external of_string : string -> bytes = "_mlml_shallow_copy"
+external to_string : bytes -> string = "_mlml_shallow_copy"
+external _get : bytes * int -> char = "_mlml_get_string"
+external _set : (bytes * int -> char) -> unit = "_mlml_set_string"
+
+let get s n = _get (s, n)
+let set s n c = _set (s, n, c)
