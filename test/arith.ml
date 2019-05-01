d139e35d3959d0907c666e82dc89a433cc751893
coord.e <me@coord-e.com>
2019-05-01 15:59:38 +0000

    Add: Test: Add some test cases

--- test/arith.ml
+++ test/arith.ml
@@ -2,5 +2,9 @@ let () =
   Tester.expr "13 + 5 * 10";
   Tester.expr "(2 + 5) * 6";
   Tester.expr "14 * 2 - 3 * 5";
-  Tester.expr "1+2+3+4+5+6+7*2+8"
+  Tester.expr "1+2+3+4+5+6+7*2+8";
+  Tester.expr "4 / 2";
+  Tester.expr "12 / 5 + 3";
+  Tester.expr "12 mod 5 * 14";
+  Tester.expr "8 + 3 mod 12 + 4"
 ;;
