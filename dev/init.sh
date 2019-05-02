a759054460c8b690b4cf8521af34e0239ddea98c
coord.e <me@coord-e.com>
2019-05-02 12:21:37 +0000

    Change: Share the build dir

--- dev/init.sh
+++ dev/init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-[ -z "$(docker image ls -q mlml-dev)" ] && docker build "$(dirname $0)" -t mlml-dev
+[ -z "$(sudo docker image ls -q mlml-dev)" ] && sudo docker build "$(dirname $0)" -t mlml-dev --build-arg LOCAL_UID=$(id -u $USER) --build-arg LOCAL_GID=$(id -g $USER)
+
+function exec_docker() {
+  sudo docker run --rm -t -v $(pwd):/home/opam/src mlml-dev "$@"
+}
