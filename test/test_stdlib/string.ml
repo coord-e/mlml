2b15ae649cbbd6ac7755fb3a7535eb9b71de514f
coord.e <me@coord-e.com>
2019-05-02 15:17:44 +0000

    Add: Stdlib; Implement some functions in `String` module

--- test/test_stdlib/string.ml
+++ test/test_stdlib/string.ml
@@ -6,3 +6,42 @@ let make n c = Bytes.make n c |> Bytes.to_string
 let init n c = Bytes.init n c |> Bytes.to_string
 let blit = Bytes.blit_string
 let sub s = Bytes.sub_string (Bytes.of_string s)
+
+let map f str =
+  let aux i = f str.[i] in
+  init (length str) aux
+;;
+
+let mapi f str =
+  let aux i = f i str.[i] in
+  init (length str) aux
+;;
+
+let rec concat d = function [] -> "" | h :: t -> h ^ d ^ concat d t
+
+(* does not exists in official stdlib *)
+let get_opt str i = if length str > i then Some str.[i] else None
+
+let index_opt str chr =
+  let rec aux i =
+    match get_opt str i with
+    | Some c when c = chr -> Some i
+    | Some _ -> aux (i + 1)
+    | None -> None
+  in
+  aux 0
+;;
+
+let split_on_char c str =
+  let rec aux str =
+    match index_opt str c with
+    | Some i ->
+      let before = sub str 0 i in
+      let p = i + 1 in
+      let after = sub str p (length str - p) in
+      let l = aux after in
+      before :: l
+    | None -> [str]
+  in
+  aux str
+;;
