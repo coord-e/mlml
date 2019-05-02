a819edafa2b9511cd502c1f6f5c7d946c5e6c213
coord.e <me@coord-e.com>
2019-05-02 12:38:32 +0000

    Change: Use ./dev scripts in CI

--- dev/init.sh
+++ dev/init.sh
@@ -7,5 +7,6 @@ readonly IMAGE_NAME=mlml-dev
 [ -z "$(sudo docker image ls -q ${IMAGE_NAME})" ] && sudo docker build "$(dirname $0)" -t ${IMAGE_NAME} --build-arg LOCAL_UID=$(id -u $USER) --build-arg LOCAL_GID=$(id -g $USER)
 
 function exec_docker() {
-  sudo docker run --rm -t -v $(pwd):/home/opam/src ${IMAGE_NAME} "$@"
+  # TRAVIS_JOB_ID is for CI environment
+  sudo docker run --rm -t -v $(pwd):/home/opam/src -e TRAVIS_JOB_ID=${TRAVIS_JOB_ID:-} ${IMAGE_NAME} "$@"
 }
