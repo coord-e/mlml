08155c2668c73aeefa762e88a11aad7e4dc1c1d7
coord.e <me@coord-e.com>
2019-05-02 12:45:19 +0000

    Change: Dev: Enable to run on an environment with non-root docker

--- dev/init.sh
+++ dev/init.sh
@@ -4,9 +4,15 @@ set -euo pipefail
 
 readonly IMAGE_NAME=mlml-dev
 
-[ -z "$(sudo docker image ls -q ${IMAGE_NAME})" ] && sudo docker build "$(dirname $0)" -t ${IMAGE_NAME} --build-arg LOCAL_UID=$(id -u $USER) --build-arg LOCAL_GID=$(id -g $USER)
+if docker version &> /dev/null; then
+  readonly DOCKER=docker
+else
+  readonly DOCKER="sudo docker"
+fi
+
+[ -z "$($DOCKER image ls -q ${IMAGE_NAME})" ] && $DOCKER build "$(dirname $0)" -t ${IMAGE_NAME} --build-arg LOCAL_UID=$(id -u $USER) --build-arg LOCAL_GID=$(id -g $USER)
 
 function exec_docker() {
   # TRAVIS_JOB_ID is for CI environment
-  sudo docker run --rm -t -v $(pwd):/home/opam/src -e TRAVIS_JOB_ID=${TRAVIS_JOB_ID:-} ${IMAGE_NAME} "$@"
+  $DOCKER run --rm -t -v $(pwd):/home/opam/src -e TRAVIS_JOB_ID=${TRAVIS_JOB_ID:-} ${IMAGE_NAME} "$@"
 }
