ec2bd8a31f2db3f6be54e671ed8017f7af3f9f3d
coord.e <me@coord-e.com>
2019-05-02 07:51:40 +0000

    Add: Test: Add test cases of array

---
+++ test/array_.ml
@@ -0,0 +1,44 @@
+let () =
+  Tester.f {|
+let ary = [|1; 2; 3|] in
+print_int ary.[1]
+  |};
+  Tester.f {|
+let a = [|1; 2; 3|] in
+let [|x; y; z|] = a in
+print_int (x * y + z)
+  |};
+  Tester.f
+    {|
+let a = [| [| 1; 2 |]; [| 3; 4 |] |] in
+let [| a; [| b; c |] |] = a in
+print_int (b + c);
+print_int a.[0]
+  |};
+  Tester.f
+    {|
+let a = [| 1; 2; 3; 4 |] in
+print_int a.[2];
+a.[2] <- 10;
+print_int a.[2]
+  |};
+  Tester.f
+    {|
+let a = [| "a"; "b"; "c" |] in
+let b = a in
+a.[2] <- "d";
+print_string b.[2]
+  |};
+  Tester.f
+    {|
+let f = function
+  | [| a |] -> a
+  | [| a; b |] -> a * b
+  | _ -> 1200
+;;
+
+f [| 2; 13 |];
+f [| 3 |];
+f [| 3; 243; 234 |]
+  |}
+;;
