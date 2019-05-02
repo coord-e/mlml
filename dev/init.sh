1dea47fe072adb550cbedd70093c36405a813457
coord.e <me@coord-e.com>
2019-05-02 12:32:47 +0000

    Add: Name as IMAGE_NAME

--- dev/init.sh
+++ dev/init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-[ -z "$(sudo docker image ls -q mlml-dev)" ] && sudo docker build "$(dirname $0)" -t mlml-dev --build-arg LOCAL_UID=$(id -u $USER) --build-arg LOCAL_GID=$(id -g $USER)
+set -euo pipefail
+
+readonly IMAGE_NAME=mlml-dev
+
+[ -z "$(sudo docker image ls -q ${IMAGE_NAME})" ] && sudo docker build "$(dirname $0)" -t ${IMAGE_NAME} --build-arg LOCAL_UID=$(id -u $USER) --build-arg LOCAL_GID=$(id -g $USER)
 
 function exec_docker() {
-  sudo docker run --rm -t -v $(pwd):/home/opam/src mlml-dev "$@"
+  sudo docker run --rm -t -v $(pwd):/home/opam/src ${IMAGE_NAME} "$@"
 }
