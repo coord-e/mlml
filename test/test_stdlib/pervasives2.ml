9a6eb01ac38b2cb798e84aca78f3c3bc3b342a45
coord.e <me@coord-e.com>
2019-05-02 04:13:35 +0000

    Fix: Stdlib: Workaround to the failure of bytes

--- test/test_stdlib/pervasives2.ml
+++ test/test_stdlib/pervasives2.ml
@@ -1,8 +1,8 @@
 let string_of_int i =
-  let char_of_digit d = char_of_int (int_of_char '0' + d) in
+  let digit_char d = char_of_int (int_of_char '0' + d) in
   let rec len = function i when i < 10 -> 1 | i -> 1 + len (i / 10) in
   let rec digit_of n = function
-    | 0 -> char_of_digit @@ (n mod 10)
+    | 0 -> digit_char @@ (n mod 10)
     | i -> digit_of (n / 10) (i - 1)
   in
   let l = len i in
