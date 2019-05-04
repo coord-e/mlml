c3a4f9a440e3a4d43be7bc60392103c3c2769de2
coord.e <me@coord-e.com>
2019-05-04 17:07:32 +0000

    Add: TEst: Add some test cases of negative integers

--- test/arith.ml
+++ test/arith.ml
@@ -7,10 +7,17 @@ let () =
   Tester.expr "12 / 5 + 3";
   Tester.expr "12 mod 5 * 14";
   Tester.expr "8 + 3 mod 12 + 4";
+  Tester.expr "-10";
+  Tester.expr "5-10";
+  Tester.expr "-(5-10)";
+  Tester.expr "-5-10*2";
+  Tester.expr "-0";
   Tester.bool_expr "1 > 2";
   Tester.bool_expr "133 < 123";
   Tester.bool_expr "42 > 42";
   Tester.bool_expr "2 < 42";
+  Tester.bool_expr "2 < -42";
+  Tester.bool_expr "-10 < -42";
   Tester.bool_expr "42 >= 42";
   Tester.bool_expr "4 <= 5";
   Tester.bool_expr "5 <= 5";
