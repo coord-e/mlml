2df444170d5c7903f4760823c706ec3959c0dd66
coord.e <me@coord-e.com>
2019-05-05 16:12:37 +0000

    Add: Bundler: Bundle stdlibs in bundler

--- lib/bundler/bundler.ml
+++ lib/bundler/bundler.ml
@@ -1,8 +1,43 @@
+let stdlib_dir =
+  match Sys.getenv_opt "MLML_STDLIB_DIR" with Some d -> d | None -> "../../../stdlib"
+;;
+
+let stdlibs = ["sys"; "array"; "bytes"; "char"; "string"; "printf"; "list"; "hashtbl"]
+
+let collect_libs dir =
+  let read file =
+    let ic = open_in @@ Printf.sprintf "%s/%s" dir file in
+    let content = really_input_string ic @@ in_channel_length ic in
+    close_in ic;
+    let name = String.split_on_char '.' file |> List.hd in
+    name, content
+  in
+  Array.map read (Sys.readdir dir) |> Array.to_list
+;;
+
+let bundle_libs libs =
+  let aux (name, content) =
+    let mod_name = String.capitalize_ascii name in
+    Printf.sprintf "module %s = struct\n%s\nend" mod_name content
+  in
+  List.map aux libs |> String.concat "\n"
+;;
+
 let bundle_file file =
   let ic = open_in file in
   let content = really_input_string ic @@ in_channel_length ic in
   close_in ic;
-  let tree = Lexer.f content |> Parser.Compilation_unit.f in
+  let libs = collect_libs stdlib_dir in
+  let p, libs = List.partition (fun (name, _) -> name = "pervasives") libs in
+  let p2, libs = List.partition (fun (name, _) -> name = "pervasives2") libs in
+  (* TODO: Remove these hacks *)
+  let libs = List.map (fun x -> x, List.assoc x libs) stdlibs |> bundle_libs in
+  let p = bundle_libs p in
+  let p2 = bundle_libs p2 in
+  let source =
+    Printf.sprintf "%s;;open Pervasives;;%s;;%s;;open Pervasives2;;%s" p libs p2 content
+  in
+  let tree = Lexer.f source |> Parser.Compilation_unit.f in
   tree
 ;;
 
