c4262975ce3484533a9fc8bb69eb383133713330
coord.e <me@coord-e.com>
2019-05-05 16:17:17 +0000

    Change: Test: Change tester to use mlml's bundler

--- test/tester.ml
+++ test/tester.ml
@@ -1,11 +1,5 @@
 open Mlml
 
-(* TODO: replace test stdlib with real stdlib *)
-let stdlib_dir =
-  match Sys.getenv_opt "MLML_STDLIB_DIR" with Some d -> d | None -> "../../../stdlib"
-;;
-
-let stdlibs = ["sys"; "array"; "bytes"; "char"; "string"; "printf"; "list"; "hashtbl"]
 let input_line_opt ch = try Some (input_line ch) with End_of_file -> None
 
 let input_all ch =
@@ -31,39 +25,14 @@ let open_and_read_result cmd =
   result
 ;;
 
-let collect_libs dir =
-  let read file =
-    let ic = open_in @@ Printf.sprintf "%s/%s" dir file in
-    let content = really_input_string ic @@ in_channel_length ic in
-    close_in ic;
-    let name = String.split_on_char '.' file |> List.hd in
-    name, content
-  in
-  Array.map read (Sys.readdir dir) |> Array.to_list
-;;
-
-let bundle_libs libs =
-  let aux (name, content) =
-    let mod_name = String.capitalize_ascii name in
-    Printf.sprintf "module %s = struct\n%s\nend" mod_name content
-  in
-  List.map aux libs |> String.concat "\n"
-;;
-
 let exec_with_mlml source =
-  let libs = collect_libs stdlib_dir in
-  let p, libs = List.partition (fun (name, _) -> name = "pervasives") libs in
-  let p2, libs = List.partition (fun (name, _) -> name = "pervasives2") libs in
-  (* TODO: Remove these hacks *)
-  let libs = List.map (fun x -> x, List.assoc x libs) stdlibs |> bundle_libs in
-  let p = bundle_libs p in
-  let p2 = bundle_libs p2 in
-  let source =
-    Printf.sprintf "%s;;open Pervasives;;%s;;%s;;open Pervasives2;;%s" p libs p2 source
-  in
+  let ml_file = Filename.temp_file "." ".ml" in
+  let oc = open_out ml_file in
+  Printf.fprintf oc "%s\n" source;
+  close_out oc;
   let as_file = Filename.temp_file "." ".s" in
   let oc = open_out as_file in
-  Printf.fprintf oc "%s\n" @@ Compile.f source;
+  Printf.fprintf oc "%s\n" @@ Compile.f ml_file;
   close_out oc;
   let exec_file = Filename.temp_file "." "" in
   let ret_code = Sys.command @@ Printf.sprintf "gcc %s -o %s" as_file exec_file in
