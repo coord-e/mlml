dd8424896d28e6471a006ae2b327e499fddb04dd
coord.e <me@coord-e.com>
2019-05-06 13:38:05 +0000

    Add: Bundler: Detect unbound modules

--- lib/bundler/find_deps.ml
+++ lib/bundler/find_deps.ml
@@ -5,64 +5,84 @@ module Mod = Tree.Module
 module Path = Tree.Path
 module Pat = Tree.Pattern
 
-let deps_of_path path =
-  if Path.is_single path then SS.empty else SS.singleton @@ Path.head path
-;;
-
 type mutable_set = {mutable data : string SS.t}
 
 let empty () = {data = SS.empty}
 let union s x = s.data <- SS.union x s.data
-let vars s path _ = union s @@ deps_of_path path
+let add s x = s.data <- SS.add x s.data
+
+type env = mutable_set
+
+let deps_of_module_path env path =
+  let h = Path.head path in
+  if SS.mem h env.data then SS.empty else SS.singleton h
+;;
+
+let deps_of_path env path =
+  match Path.is_single path with
+  | true -> SS.empty
+  | false -> deps_of_module_path env path
+;;
+
+let vars env s path _ = union s @@ deps_of_path env path
 let binds x _ = x
 
-let type_expression ex =
+let type_expression env ex =
   let s = empty () in
-  let _ = TyExpr.apply_on_names (vars s) binds ex in
+  let _ = TyExpr.apply_on_names (vars env s) binds ex in
   s.data
 ;;
 
-let type_definition = function
+let type_definition env = function
   | Mod.Variant l ->
-    let aux = function _, Some x -> type_expression x | _, None -> SS.empty in
+    let aux = function _, Some x -> type_expression env x | _, None -> SS.empty in
     List.map aux l |> List.fold_left SS.union SS.empty
   | Mod.Record l ->
-    let aux (_, _, x) = type_expression x in
+    let aux (_, _, x) = type_expression env x in
     List.map aux l |> List.fold_left SS.union SS.empty
-  | Mod.Alias x -> type_expression x
+  | Mod.Alias x -> type_expression env x
 ;;
 
-let pattern p =
+let pattern env p =
   let s = empty () in
-  let _ = Pat.apply_on_names (vars s) binds p in
+  let _ = Pat.apply_on_names (vars env s) binds p in
   s.data
 ;;
 
-let expression e =
+let expression env e =
   let s = empty () in
-  let _ = Expr.apply_on_names (vars s) binds e in
+  let _ = Expr.apply_on_names (vars env s) binds e in
   s.data
 ;;
 
-let rec definition = function
+let rec definition env = function
   | Mod.LetAnd (_, l) ->
     let aux = function
       | Expr.VarBind (p, b) | Expr.FunBind (_, p, b) ->
-        SS.union (pattern p) (expression b)
+        SS.union (pattern env p) (expression env b)
     in
     List.map aux l |> List.fold_left SS.union SS.empty
   | Mod.TypeDef l ->
-    let aux (_, _, def) = type_definition def in
+    let aux (_, _, def) = type_definition env def in
     List.map aux l |> List.fold_left SS.union SS.empty
-  | Mod.Module (_, Mod.Path path) -> deps_of_path path
-  | Mod.Module (_, Mod.Struct l) -> module_items l
-  | Mod.Open path -> deps_of_path path
-  | Mod.External (_, ty, _) -> type_expression ty
+  | Mod.Module (name, Mod.Path path) ->
+    let s = deps_of_module_path env path in
+    add env name;
+    s
+  | Mod.Module (name, Mod.Struct l) ->
+    let s = module_items env l in
+    add env name;
+    s
+  | Mod.Open path -> deps_of_module_path env path
+  | Mod.External (_, ty, _) -> type_expression env ty
 
-and module_item = function
-  | Mod.Expression expr -> expression expr
-  | Mod.Definition defn -> definition defn
+and module_item env = function
+  | Mod.Expression expr -> expression env expr
+  | Mod.Definition defn -> definition env defn
 
-and module_items l = List.map module_item l |> List.fold_left SS.union SS.empty
+and module_items env l = List.map (module_item env) l |> List.fold_left SS.union SS.empty
 
-let f = module_items
+let f l =
+  let env = empty () in
+  module_items env l
+;;
