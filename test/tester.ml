7afb280aa81d4c863ef0883a9a2ad3265cf6be72
coord.e <me@coord-e.com>
2019-05-09 04:34:40 +0000

    [WIP] Add: Test: Add a simplest boilerplate of test code

--- test/tester.ml
+++ test/tester.ml
@@ -25,14 +25,10 @@ let open_and_read_result cmd =
   result
 ;;
 
-let exec_with_mlml source =
-  let ml_file = Filename.temp_file "." ".ml" in
-  let oc = open_out ml_file in
-  Printf.fprintf oc "%s\n" source;
-  close_out oc;
+let exec_with_mlml_file file =
   let as_file = Filename.temp_file "." ".s" in
   let oc = open_out as_file in
-  Printf.fprintf oc "%s\n" @@ Compile.f ml_file;
+  Printf.fprintf oc "%s\n" @@ Compile.f file;
   close_out oc;
   let exec_file = Filename.temp_file "." "" in
   let ret_code = Sys.command @@ Printf.sprintf "gcc %s -o %s" as_file exec_file in
@@ -40,12 +36,22 @@ let exec_with_mlml source =
   open_and_read_result exec_file
 ;;
 
+let exec_with_mlml source =
+  let ml_file = Filename.temp_file "." ".ml" in
+  let oc = open_out ml_file in
+  Printf.fprintf oc "%s\n" source;
+  close_out oc;
+  exec_with_mlml_file ml_file
+;;
+
+let exec_with_ocaml_file file = open_and_read_result @@ "ocaml " ^ file
+
 let exec_with_ocaml source =
   let ml_file = Filename.temp_file "." ".ml" in
   let oc = open_out ml_file in
   Printf.fprintf oc "%s\n" source;
   close_out oc;
-  open_and_read_result @@ "ocaml " ^ ml_file
+  exec_with_ocaml_file ml_file
 ;;
 
 let f source =
@@ -65,4 +71,13 @@ let expr source =
 let bool_expr source =
   let source = Printf.sprintf "if (%s) then 1 else 0" source in
   expr source
+;;
+
+let file path =
+  let mlml_result = exec_with_mlml_file path in
+  let ocaml_result = exec_with_ocaml_file path in
+  if not (mlml_result = ocaml_result)
+  then (
+    Printf.eprintf "mlml: (%s)\nocaml: (%s)\n" mlml_result ocaml_result;
+    failwith "assertion failed" )
 ;;
