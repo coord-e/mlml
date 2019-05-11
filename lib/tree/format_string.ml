38949bc8f355a60c3b8276971bb99a1f898112ef
coord.e <me@coord-e.com>
2019-05-11 16:13:06 +0000

    Fix: Tree: Fix format string printing notation to prevent problems in self-hosting

--- lib/tree/format_string.ml
+++ lib/tree/format_string.ml
@@ -6,9 +6,9 @@ type kind =
 
 let string_of_kind = function
   | Const s -> s
-  | Int -> "%d"
-  | Char -> "%c"
-  | String -> "%s"
+  | Int -> "(d)"
+  | Char -> "(c)"
+  | String -> "(s)"
 ;;
 
 let string_of_format_string l = List.map string_of_kind l |> String.concat ""
