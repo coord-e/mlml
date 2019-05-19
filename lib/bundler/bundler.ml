020c8f48c30d5d81ccd95a29aac50fb25e973b2e
coord.e <me@coord-e.com>
2019-05-19 12:04:22 +0000

    Add: Bundler: Bundle scoped dependencies

--- lib/bundler/bundler.ml
+++ lib/bundler/bundler.ml
@@ -4,14 +4,21 @@ module ModCache = Modules_cache
 module SS = Tree.Simple_set
 module DepTree = Dependency_tree
 module Build = Build_tree
+module Find = Find_projects
 
-let bundle_libs cache libs = List.rev_map (ModCache.get cache) libs |> List.flatten
-
-let bundle_file cache file =
-  Build.build_tree_root cache file |> DepTree.collapse |> bundle_libs cache
+let rec bundle_libs cache libs =
+  let f = function
+    | DepTree.Entry p -> ModCache.get cache p
+    | DepTree.Scoped (name, l) ->
+      let name = String.capitalize_ascii name in
+      let l = bundle_libs cache l in
+      [Mod.Definition (Mod.Module (name, Mod.Struct l))]
+  in
+  List.rev_map f libs |> List.flatten
 ;;
 
 let f file =
   let cache = ModCache.empty () in
-  bundle_file cache file
+  let projects = Find.find_project_root file |> Find.find_projects |> SS.elements in
+  Build.build_tree_root cache projects file |> DepTree.collapse |> bundle_libs cache
 ;;
