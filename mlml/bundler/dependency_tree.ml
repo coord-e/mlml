4b29cfaf8394d977ba224ad7ccea08faa1c89917
coord.e <me@coord-e.com>
2019-05-20 22:26:39 +0000

    Fix: Bundler: Fix to bundle submodule properly

--- mlml/bundler/dependency_tree.ml
+++ mlml/bundler/dependency_tree.ml
@@ -11,13 +11,8 @@ and 'a dep_list = 'a dep list
 let merge_list a b =
   let rec aux acc = function
     | h :: t ->
-      let t =
-        match h with
-        | Entry _ ->
-          let f x = x <> h in
-          List.filter f t
-        | Scoped _ -> t
-      in
+      let f x = x <> h in
+      let t = List.filter f t in
       aux (h :: acc) t
     | [] -> acc
   in
@@ -28,14 +23,18 @@ let merge_list a b =
 let rec collapse_list l = List.map collapse l |> List.fold_left merge_list []
 
 and upper_lower l =
-  let aux = function Node (file, l) -> [Entry file], l | t -> collapse t, [] in
-  let upper, lower = List.map aux l |> List.split in
-  List.flatten upper, List.flatten lower
+  let aux = function Node (file, _) -> file | Submodule (name, _) -> name in
+  let under_names = List.map aux l in
+  let l = collapse_list l in
+  let f = function Entry name | Scoped (name, _) -> List.mem name under_names in
+  List.partition f l
 
 and collapse = function
   | Node (file, []) -> [Entry file]
   | Node (file, l) -> Entry file :: collapse_list l
   | Submodule (name, l) ->
+    (* `upper` is what is expected to be accessible as if it is under the name *)
+    (* `lower` is the other, non-related things                                *)
     let upper, lower = upper_lower l in
-    Scoped (name, upper) :: collapse_list lower
+    Scoped (name, upper) :: lower
 ;;
