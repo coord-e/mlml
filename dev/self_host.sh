afe401ca2d2b2c8a4681be69c6e16f9511cc222b
coord.e <me@coord-e.com>
2019-05-22 15:49:21 +0000

    Add: Dev: Confirm the self-hosing with comparing output of 2nd and 3rd

--- dev/self_host.sh
+++ dev/self_host.sh
@@ -20,19 +20,31 @@ function compile () {
 
   cmd "$@ bin/mlmlc.ml > \"$ASMOUT\""
   cmd "gcc \"$ASMOUT\" -lgc -o \"$EXECOUT\""
+  md5sum "$ASMOUT"
 }
 
 function main () {
   readonly GEN1="$WORKDIR/mlml1"
   readonly GEN2="$WORKDIR/mlml2"
+  readonly GEN3="$WORKDIR/mlml3"
 
   info "Compiling mlml with ocaml"
-  compile $GEN1 dune exec bin/mlmlc.exe
+  compile $GEN1 dune exec bin/mlmlc.exe > /dev/null
   info "Successfully compiled 1st-gen compiler: $GEN1"
 
-  info "Compiling mlml with mlml"
-  compile $GEN2 $GEN1
+  info "Compiling mlml with mlml (1st generation)"
+  readonly GEN2_HASH=$(compile $GEN2 $GEN1)
   info "Successfully compiled 2nd-gen compiler: $GEN2"
+
+  info "Compiling mlml with mlml (2nd generation)"
+  readonly GEN3_HASH=$(compile $GEN3 $GEN2)
+  info "Successfully compiled 3rd-gen compiler: $GEN3"
+
+  if [ "$GEN2_HASH" = "$GEN3_HASH" ]; then
+    info "SELF HOSTING SUCCESSFUL"
+  else
+    info "Self hosting failed (output mismatch between 2nd and 3rd)"
+  fi
 }
 
 main
