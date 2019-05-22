416f2257c4e25ec7a293b0f957b61ff28c035379
coord.e <me@coord-e.com>
2019-05-22 16:13:52 +0000

    Fix: Dev: Fix the stack size

--- dev/init.sh
+++ dev/init.sh
@@ -20,8 +20,8 @@ fi
 function exec_docker() {
   # TRAVIS_JOB_ID is for CI environment
   $DOCKER run --rm -t \
-    --ulimit stack=3048576 \
+    --ulimit stack=4294967296 \
     -v $(pwd):/home/opam/src \
-    -e TRAVIS_JOB_ID=${TRAVIS_JOB_ID:-} ${IMAGE_NAME} \
-    "$@"
+    -e TRAVIS_JOB_ID=${TRAVIS_JOB_ID:-} \
+    ${IMAGE_NAME} "$@"
 }
